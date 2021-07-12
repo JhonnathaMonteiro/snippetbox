a3c01b5cec790905b1ab219af601f0384bd042e8
Jhonnatha <jhonnatha.am@gmail.com>
2021-07-12 09:16:24 -0300

    3.0

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,10 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	filerServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", filerServer))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatalln(err)
