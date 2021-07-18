188dd3d6ea229044eca777db1da5d01631686071
Jhonnatha <jhonnatha.am@gmail.com>
2021-07-18 19:20:12 -0300

    pre 3.3

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "\033[36mINFO\033[0m\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "\033[31mERROR\033[0m\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -14,7 +23,13 @@ func main() {
 	filerServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", filerServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatalln(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatalln(err)
 }
