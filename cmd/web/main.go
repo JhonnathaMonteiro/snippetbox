b5add993d981c3f3027650be31a7f4243db054c3
Jhonnatha <jhonnatha.am@gmail.com>
2021-07-28 08:41:31 -0300

    4.5

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,35 +1,63 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jhonnathamonteiro/snippetbox/pkg/models/mysql"
 )
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
+}
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL database DSN")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "\033[36mINFO\033[0m\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "\033[31mERROR\033[0m\t", log.Ldate|log.Ltime|log.Lshortfile)
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	filerServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", filerServer))
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
+	}
 
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatalln(err)
+}
+
+// The openDB() function wraps sql.Open() and reruns a sql.DB connection pool
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	// db, err := sql.Open("mysql", "root:my-password-pw@tcp(192.168.0.33:3306)/snippetbox")
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
